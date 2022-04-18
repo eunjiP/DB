f9f56f5133bae8c6ebca5820a84091a514f81667
eunjiP <101936519+eunjiP@users.noreply.github.com>
2022-04-18 15:31:14 +0900

    20220422에 예정인 스터디활동

---
+++ study_homework/naver_join_page/회원가입테이블쿼리.sql
@@ -0,0 +1,18 @@
+/*네이버 회원가입 페이지를 보고 테이블 구상해보기*/
+
+CREATE TABLE n_join (
+	user_id varCHAR(20) PRIMARY KEY,
+	user_pw varCHAR(20) NOT NULL
+);
+
+CREATE TABLE user_infor (
+	user_id varCHAR(20) PRIMARY KEY,
+	user_name varCHAR(10) NOT NULL,
+	b_year INT UNSIGNED NOT NULL,
+	b_month INT UNSIGNED NOT NULL,
+	b_day INT UNSIGNED NOT NULL,
+	gender CHAR(1) NOT NULL,
+	email VARCHAR(30),
+	hp INT UNSIGNED NOT null
+);
+
